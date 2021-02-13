e52853d44a638c3673dc50942891e5dab308014e
josephlegrand291 <josephlegrand@291gmail.com>
2021-02-14 01:41:58 +0200

    Fixed cors error

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const { json } = require("body-parser");
 //Creates an express application
 const app = express();
 app.use(json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/sentence", sentenceRoute);
 app.use("/word", wordRoute);
 
